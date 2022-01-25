0bbb28bd9b16c64f10ce42d26a0654dc312dc5c2
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-01-25 18:17:49 +0000

    Create templates for authentication
    - Implement material-ui login page template
    - Implement materoail-ui signup page template
    - Add routes for login and sign up to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './templates/Login';
+import SignUp from './templates/SignUp';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Authentication">
+          <Router>
+            <Switch>
+              <Route exact path="/"> <Login /> </Route>
+              <Route path="/sign-up"> <SignUp /> </Route>
+            </Switch>
+          </Router>
+        </div>
+      </div> 
+    </Router>
+    
   );
 }
 
