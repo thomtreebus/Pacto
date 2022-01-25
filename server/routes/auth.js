7e08178aa54108ea6c287e5d5e479580b3759b5f
Kieran Woolley <kieran.woolley1@gmail.com>
2022-01-25 17:46:49 +0000

    Add basic auth routes for login, signup and logout

---
+++ server/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.post('/signup', (req,res) => {
+  const { firstName, lastName, uniEmail, password } = req.body;
+
+  try {
+    const user = await User.create({ firstName, lastName, uniEmail, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  }
+  catch(err) {
+    res.status(400);
+  }
+});
+
+router.post('/login', (req,res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } 
+  catch (err) {
+    res.status(400).json();
+  }
+
+});
+
+router.get('/logout', (req,res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/');
+});
+
+module.exports = router;
