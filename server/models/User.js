5ad83657edc4bb6608cfbce21cf543d896637dc0
Kieran Woolley <kieran.woolley1@gmail.com>
2022-01-25 17:21:49 +0000

    Create user schema and model

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true 
+  },
+  lastName: {
+    type: String,
+    required: true 
+  },
+  personalEmail: {
+    type: String,
+    required: false 
+  },
+  uniEmail: {
+    type: String,
+    required: true 
+  },
+  password: {
+    type: String,
+    required: true 
+  },
+  course: {
+    type: String,
+    required: false 
+  }
+});
+
+module.exports = mongoose.model('Userrs, UserSchema');
