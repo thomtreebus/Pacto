66b7d9b778dfb9542d19c24c6306baf31b502b4e
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-01-26 13:47:37 +0000

    Add test framework code for server

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,6 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 describe("Actions script", () => {
+	beforeAll(async () => {
+		await mongoose.connect(process.env.TEST_DB_CONNECTION_URL);
+	});
+
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+
+	afterEach(async () => {
+		// Empty the database after each test
+		// await ModelName.deleteMany({});
+	});
+
 	it("passes the test", () => {
-		console.log(process.env.TEST_DB_CONNECTION_URL);
 		expect(true).toBe(true);
 	});
 });
