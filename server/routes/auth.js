381dc9e25b9d53f8e1624e3e5a5bfeaaf4b309ab
Kieran Woolley <kieran.woolley1@gmail.com>
2022-01-26 17:15:12 +0000

    Implement password hashing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 // create json web token
 const maxAge = 5 * 24 * 60 * 60; // 5 days
@@ -15,7 +17,9 @@ router.post('/signup', async (req,res) => {
   const { firstName, lastName, uniEmail, password } = req.body;
 
   try {
-    const user = await User.create({ firstName, lastName, uniEmail, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({ firstName, lastName, uniEmail, password:hashedPassword });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({user});
@@ -30,19 +34,19 @@ router.post('/login', async (req,res) => {
 
   try {
     const user = await User.findOne({ uniEmail });
-    console.log("here")  
     if (user) {
       const auth = await bcrypt.compare(password, user.password);
       if (auth) {
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(200).json({ user: user._id });
-      }
+        return;
+      } 
     }
     throw Error('incorrect credentials');
   } 
   catch (err) {
-    res.status(400).json({err});
+    res.status(400).json({errors: [err]});
   }
 
 });
