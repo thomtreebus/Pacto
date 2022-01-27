d54012bd0550c24ec760f98954980984c3958564
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-01-27 13:38:19 +0000

    Remove unused import statements fron SideBar.jsx

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,30 +1,19 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Avatar from '@mui/material/Avatar';
 import PactoIcon from '../assets/pacto-logo.ico';
 import FeedIcon from '@mui/icons-material/Feed';
 import PactIcon from '@mui/icons-material/Forum';
 import FriendsIcon from '@mui/icons-material/People';
 import UniversityIcon from '@mui/icons-material/School';
-import { deepOrange, deepPurple } from '@mui/material/colors';
-import { ListItemButton } from '@mui/material';
+import { deepPurple } from '@mui/material/colors';
 
 const drawerWidth = 240;
 
