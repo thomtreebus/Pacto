291c97c179504ff58826d0d5f4e394bd99d8d25a
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-01-27 11:54:15 +0000

    Fix textfield placeholder text bug

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-// import Link from "@mui/material/Link";
 import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
