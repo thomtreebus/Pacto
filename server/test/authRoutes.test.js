601cb07581bb9cc54efffe86af7c04256d8cae30
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-01-27 21:58:20 +0000

    Add tests for GET /me

--- server/test/authRoutes.test.js
+++ server/test/authRoutes.test.js
@@ -6,6 +6,7 @@ const supertest = require("supertest");
 const bcrypt = require("bcrypt");
 const app = require("../app");
 var Cookies = require("expect-cookies");
+const { createToken } = require("../controllers/authController");
 
 dotenv.config();
 
@@ -24,15 +25,15 @@ const VERIFY_SUCCESS_RESPONSE_TEXT = "Success! You may now close this page."; //
 const TEST_USER_EMAIL = "pac.to@kcl.ac.uk";
 const generateTestUser = async () => {
 	const salt = await bcrypt.genSalt(SALT_ROUNDS);
-			const hashedPassword = await bcrypt.hash("Password123", salt);
-			const user = await User.create({
-				firstName: "pac",
-				lastName: "to",
-				uniEmail: TEST_USER_EMAIL,
-				password: hashedPassword,
-			});
-			return user;
-}
+	const hashedPassword = await bcrypt.hash("Password123", salt);
+	const user = await User.create({
+		firstName: "pac",
+		lastName: "to",
+		uniEmail: TEST_USER_EMAIL,
+		password: hashedPassword,
+	});
+	return user;
+};
 
 describe("Authentication routes", () => {
 	beforeAll(async () => {
@@ -55,7 +56,11 @@ describe("Authentication routes", () => {
 			await user.save();
 		});
 
-		async function isInvalidCredentials(uniEmail, password, msg=INCORRECT_CREDENTIALS) {
+		async function isInvalidCredentials(
+			uniEmail,
+			password,
+			msg = INCORRECT_CREDENTIALS
+		) {
 			const response = await supertest(app)
 				.post("/login")
 				.send({
@@ -110,9 +115,16 @@ describe("Authentication routes", () => {
 		});
 
 		it("rejects inactive user with correct credentials", async () => {
-			await User.findOneAndUpdate({uniEmail: TEST_USER_EMAIL}, {active: false});
-			await isInvalidCredentials("pac.to@kcl.ac.uk", "Password123", INACTIVE_ACCOUNT);
-		})
+			await User.findOneAndUpdate(
+				{ uniEmail: TEST_USER_EMAIL },
+				{ active: false }
+			);
+			await isInvalidCredentials(
+				"pac.to@kcl.ac.uk",
+				"Password123",
+				INACTIVE_ACCOUNT
+			);
+		});
 	});
 
 	describe("GET /verify", () => {
@@ -120,19 +132,18 @@ describe("Authentication routes", () => {
 			const user = await generateTestUser();
 			await EmailVerificationCode.create({
 				userId: user._id,
-				code: VERIFICATION_CODE
+				code: VERIFICATION_CODE,
 			});
 		});
 
 		// Helpers
 		async function getVerifyWithCode(code) {
-			const response = await supertest(app)
-			.get("/verify?code="+code);
+			const response = await supertest(app).get("/verify?code=" + code);
 
 			return response;
 		}
 
-		async function isResponseUnsuccessful(response, msg=INVALID_CODE) {
+		async function isResponseUnsuccessful(response, msg = INVALID_CODE) {
 			expect(response.statusCode).toBe(400);
 			expect(response.body.errors.length).toBe(1);
 			expect(response.body.errors[0].message).toBe(msg);
@@ -148,7 +159,7 @@ describe("Authentication routes", () => {
 			const response = await getVerifyWithCode(VERIFICATION_CODE);
 			isResponseSuccessful(response);
 
-			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL});
+			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL });
 			expect(user.active).toBe(true);
 		});
 
@@ -156,27 +167,90 @@ describe("Authentication routes", () => {
 			const response = await getVerifyWithCode(VERIFICATION_CODE + "gibberish");
 			isResponseUnsuccessful(response);
 
-			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL});
+			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL });
 			expect(user.active).toBe(false);
 		});
 
 		it("fails to verify inactive user with no code param", async () => {
 			const response = await getVerifyWithCode("");
-			isResponseUnsuccessful(response, MISSING_CODE)
+			isResponseUnsuccessful(response, MISSING_CODE);
 
-			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL});
+			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL });
 			expect(user.active).toBe(false);
 		});
 
 		it("identifies already used code as invalid, with user object not affected", async () => {
 			const response = await getVerifyWithCode(VERIFICATION_CODE);
 			isResponseSuccessful(response);
-			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL});
+			const user = await User.findOne({ uniEmail: TEST_USER_EMAIL });
 			expect(user.active).toBe(true);
 
 			const responseAfterSuccess = await getVerifyWithCode(VERIFICATION_CODE);
 			isResponseUnsuccessful(responseAfterSuccess);
 			expect(user.active).toBe(true);
+		});
+	});
+
+	describe("GET /me", () => {
+		it("returns a user object when logged in", async () => {
+			const password = "Password123";
+			const salt = await bcrypt.genSalt(SALT_ROUNDS);
+			const hashedPassword = await bcrypt.hash(password, salt);
+
+			const user = await User.create({
+				firstName: "pac",
+				lastName: "to",
+				uniEmail: "pac.to@kcl.ac.uk",
+				password: hashedPassword,
+			});
+
+			const token = createToken(user._id);
+
+			let response = await supertest(app)
+				.get("/me")
+				.set("Cookie", [`jwt=${token}`]);
+			expect(response.body.message).toBeDefined();
+			expect(response.body.message._id).toBeDefined();
+			expect(response.body.message.firstName).toBeDefined();
+			expect(response.body.message.lastName).toBeDefined();
+			expect(response.body.message.uniEmail).toBeDefined();
+			expect(response.body.message.password).toBeDefined();
+			expect(response.body.errors.length).toBe(0);
+		});
+
+		it("returns an error when not  logged in", async () => {
+			const password = "Password123";
+			const salt = await bcrypt.genSalt(SALT_ROUNDS);
+			const hashedPassword = await bcrypt.hash(password, salt);
+
+			const user = await User.create({
+				firstName: "pac",
+				lastName: "to",
+				uniEmail: "pac.to@kcl.ac.uk",
+				password: hashedPassword,
+			});
+
+			const token = createToken(user._id);
+
+			let response = await supertest(app)
+				.get("/me")
+				.set("Cookie", [`jwt=${token}`])
+				.expect(200);
+			expect(response.body.message).toBeDefined();
+			expect(response.body.message._id).toBeDefined();
+			expect(response.body.message.firstName).toBeDefined();
+			expect(response.body.message.lastName).toBeDefined();
+			expect(response.body.message.uniEmail).toBeDefined();
+			expect(response.body.message.password).toBeDefined();
+			expect(response.body.errors.length).toBe(0);
+		});
+
+		it("returns an error when the user is not logged in", async () => {
+			let response = await supertest(app).get("/me");
+			expect(response.body.message).toBe(null);
+			expect(response.body.errors[0].field).toBe(null);
+			expect(response.body.errors[0].message).toBe("You have to login");
+			expect(response.body.errors.length).toBe(1);
 		});
 	});
 });
