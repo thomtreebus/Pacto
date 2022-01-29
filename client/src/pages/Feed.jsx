172c02e73ab4490d0b69509b0e3d3626e2bad56b
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-01-29 00:09:12 +0000

    Add useAuth provider to feed page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useAuth } from '../providers/AuthProvider';
 
 export default function Feed() {
+  const { user } = useAuth()
+  console.log(user)
   return (
     <div></div>
   );
