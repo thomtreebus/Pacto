9d777e42bb4eccf2864bde37a91df8358e340a12
Aaron Patrick Monte <maaronpatrick@gmail.com>
2022-01-29 08:08:16 +0000

    Refactor SideBar tests to use beforeEach method

--- client/src/tests/SideBar.test.js
+++ client/src/tests/SideBar.test.js
@@ -12,44 +12,41 @@ import "@testing-library/jest-dom";
 // }
 
 describe("Check elements are rendered", () => {
-  it("should render the drawer element", () => {
+
+  beforeEach(() => {
     render(<SideBar />)
+  })
+  it("should render the drawer element", () => {
     const drawerElement = screen.getByTestId("sidebar-drawer")
     expect(drawerElement).toBeInTheDocument()
   })
 
   it("should render the toolbar element", () => {
-    render(<SideBar />)
     const toolbarElement = screen.getByTestId("sidebar-toolbar")
     expect(toolbarElement).toBeInTheDocument()
   })
     
   it("should render the drawer element", () => {
-    render(<SideBar />)
     const drawerElement = screen.getByTestId("sidebar-drawer")
     expect(drawerElement).toBeInTheDocument()
   })
 
   it("should render the user profile list element", () => {
-    render(<SideBar />)
     const profileElement = screen.getByTestId("sidebar-profile-list")
     expect(profileElement).toBeInTheDocument()
   })
 
   it("should render the user's avatar", () => {
-    render(<SideBar />)
     const avatarElement = screen.getByTestId("sidebar-avatar")
     expect(avatarElement).toBeInTheDocument()
   })
 
   it("should render the user's name", () => {
-    render(<SideBar />)
     const userNameElement = screen.getByTestId("sidebar-user-name")
     expect(userNameElement).toBeInTheDocument()
   })
 
   it("should render the university hub item", () => {
-    render(<SideBar />)
     const hubIconElement = screen.getByTestId("sidebar-hub-icon")
     expect(hubIconElement).toBeInTheDocument()
     const hubTextElement = screen.getByTestId("sidebar-hub")
@@ -57,7 +54,6 @@ describe("Check elements are rendered", () => {
   })
 
   it("should render the feed item", () => {
-    render(<SideBar />)
     const feedIconElement = screen.getByTestId("sidebar-feed-icon")
     expect(feedIconElement).toBeInTheDocument()
     const feedTextElement = screen.getByTestId("sidebar-feed")
@@ -65,7 +61,6 @@ describe("Check elements are rendered", () => {
   })
 
   it("should render the pacts item", () => {
-    render(<SideBar />)
     const pactIconElement = screen.getByTestId("sidebar-pacts-icon")
     expect(pactIconElement).toBeInTheDocument()
     const pactTextElement = screen.getByTestId("sidebar-pacts")
@@ -73,7 +68,6 @@ describe("Check elements are rendered", () => {
   })
 
   it("should render the friends item", () => {
-    render(<SideBar />)
     const friendIconElement = screen.getByTestId("sidebar-friends-icon")
     expect(friendIconElement).toBeInTheDocument()
     const friendTextElement = screen.getByTestId("sidebar-friends")
@@ -81,7 +75,6 @@ describe("Check elements are rendered", () => {
   })
 
   it("should render the friends item", () => {
-    render(<SideBar />)
     const friendIconElement = screen.getByTestId("sidebar-friends-icon")
     expect(friendIconElement).toBeInTheDocument()
     const friendTextElement = screen.getByTestId("sidebar-friends")
@@ -89,7 +82,6 @@ describe("Check elements are rendered", () => {
   })
 
   it("should render the 'Sign In' header element", () => {
-    render(<SideBar />)
     const typographyElement = screen.getByTestId("sidebar-mypacts-text")
     expect(typographyElement).toBeInTheDocument()
   })
