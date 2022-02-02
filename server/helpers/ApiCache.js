7282e9ba007af272d653207fc23ffc9eb9d5f303
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-02-02 15:42:54 +0000

    Add comments to ApiCache.js

--- server/helpers/ApiCache.js
+++ server/helpers/ApiCache.js
@@ -1,21 +1,24 @@
 const NodeCache = require("node-cache");
 const fetch = require("node-fetch");
 
-// cache lasts for 60 seconds
+// cache data lasts for 60 seconds before getting deleted.
 const apiCache = new NodeCache({stdTTL: 60});
 
 const getUrls = async (url) => {
   try {
+    // cache contains data
     if (apiCache.has(url)) {
       return apiCache.get(url)
     } else {
       const res = await fetch(url)
       const data = await res.json();
+      // data is added to cache
       apiCache.set(url, data)
       return data;
     }
   }
   catch (err){
+    // returns empty json if there was some sort of error in the process.
     return {};
   }
 }
