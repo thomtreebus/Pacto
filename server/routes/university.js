aba4836516cbe3ea61fa62e0a4b05306b20e4fc1
Kieran Woolley <kieran.woolley1@gmail.com>
2022-02-02 13:52:44 +0000

    Link university route file to controller file

--- server/routes/university.js
+++ server/routes/university.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const universityController = require("../controllers/universityController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
+
+router.get("/university", universityController.universityGet, checkAuthenticated);
+
+module.exports = router;
