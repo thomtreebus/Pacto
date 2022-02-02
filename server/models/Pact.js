0d91f18a4c029fba8d645fcf90502e7d9630f6f7
Kieran Woolley <kieran.woolley1@gmail.com>
2022-02-02 13:39:53 +0000

    Add name to pact

--- server/models/Pact.js
+++ server/models/Pact.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PactSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
   university: {
     type: Schema.Types.ObjectId,
     ref: 'University',
