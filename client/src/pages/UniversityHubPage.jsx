01627fa35adf94b37e59aa6d2bd4b8d20b8a7fb2
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-02-02 18:56:53 +0000

    Add basic university hub page

--- client/src/pages/UniversityHubPage.jsx
+++ client/src/pages/UniversityHubPage.jsx
@@ -1,8 +1,85 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
-import background from "../assets/hub-background.svg";
+import { Box, Typography, TextField, Grid } from "@mui/material";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import { useState } from "react";
+import background from "../assets/hub-background.jpg";
+import { Divider } from "@mui/material";
+import PactCard from "../components/PactCard";
 
 export default function UniversityHubPage() {
+	const [category, setCategory] = useState("all");
+
+	const tempPacts = [
+		{
+			name: "Pact1",
+			description: "No description provided",
+			category: "subjects",
+			tags: ["apple", "ball"],
+			members: ["jeff", "jhon"],
+		},
+		{
+			name: "Pact2",
+			description: "description retracted",
+			category: "courses",
+			tags: ["cat", "dog"],
+			members: ["jane"],
+		},
+		{
+			name: "Pact3",
+			description: "No description provided",
+			category: "other",
+			tags: ["elephant", "fish"],
+			members: ["alice", "bob", "charlie"],
+		},
+		{
+			name: "Pact1",
+			description: "No description provided",
+			category: "subjects",
+			tags: ["apple", "ball"],
+			members: ["jeff", "jhon"],
+		},
+		{
+			name: "Pact2",
+			description: "description retracted",
+			category: "courses",
+			tags: ["cat", "dog"],
+			members: ["jane"],
+		},
+		{
+			name: "Pact3",
+			description: "No description provided",
+			category: "other",
+			tags: ["elephant", "fish"],
+			members: ["alice", "bob", "charlie"],
+		},
+		{
+			name: "Pact1",
+			description: "No description provided",
+			category: "subjects",
+			tags: ["apple", "ball"],
+			members: ["jeff", "jhon"],
+		},
+		{
+			name: "Pact2",
+			description: "description retracted",
+			category: "courses",
+			tags: ["cat", "dog"],
+			members: ["jane"],
+		},
+		{
+			name: "Pact3",
+			description: "No description provided",
+			category: "other",
+			tags: ["elephant", "fish"],
+			members: ["alice", "bob", "charlie"],
+		},
+	];
+
+	const handleChange = (category, newCategory) => {
+		setCategory(newCategory);
+	};
+
 	return (
 		<Box
 			sx={{
@@ -26,7 +103,7 @@ export default function UniversityHubPage() {
 					flexDirection: "column",
 					justifyContent: "center",
 					alignItems: "center",
-					textShadow: "1px 2px #007FFF",
+					textShadow: "1px 1px grey",
 					color: "white",
 				}}
 			>
@@ -36,7 +113,49 @@ export default function UniversityHubPage() {
 				<Typography variant="h5" sx={{ fontWeight: "bold" }}>
 					There's a pact for everything if not make one...
 				</Typography>
+				<TextField
+					id="outlined-basic"
+					label="Search"
+					variant="filled"
+					size="small"
+					sx={{
+						backgroundColor: "white",
+						textShadow: "none",
+						margin: "10px",
+						width: "300px",
+						borderRadius: "5px",
+						overflow: "hidden",
+					}}
+				/>
 			</Box>
+			<Divider sx={{ marginTop: "15px" }} />
+			<Box>
+				<TabContext value={category}>
+					<Box
+						sx={{
+							borderBottom: 1,
+							marginBottom: "10px",
+							borderColor: "divider",
+						}}
+					>
+						<TabList onChange={handleChange} aria-label="lab API tabs example">
+							<Tab label="All" value="all" />
+							<Tab label="Subjects" value="subjects" />
+							<Tab label="Modules" value="modules" />
+							<Tab label="Other" value="other" />
+						</TabList>
+					</Box>
+				</TabContext>
+			</Box>
+			<Grid container spacing={2}>
+				{tempPacts.map((pact) => (
+					<PactCard
+						pact={pact}
+						id={pact}
+						joined={Math.floor(Math.random() * 10) > 4}
+					/>
+				))}
+			</Grid>
 		</Box>
 	);
 }
