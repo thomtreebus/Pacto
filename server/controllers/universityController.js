22f24708096ed7d17a6b2330b32b50ed6f9f93af
Kieran Woolley <kieran.woolley1@gmail.com>
2022-02-02 14:11:21 +0000

    Add get university route skeleton

--- server/controllers/universityController.js
+++ server/controllers/universityController.js
@@ -1,6 +1,13 @@
 const User = require("../models/User");
+const University = require("../models/University");
 const { jsonResponse, jsonError } = require("../helpers/responseHandlers");
 
 module.exports.universityGet = async (req, res) => {
-  
+  try{
+    const user = req.user;
+    
+  } 
+  catch (err) {
+
+  }
 }
--- server/routes/university.js
+++ server/routes/university.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const universityController = require("../controllers/universityController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 
-router.get("/university", universityController.universityGet, checkAuthenticated);
+router.get("/university", checkAuthenticated, universityController.universityGet);
 
 module.exports = router;
