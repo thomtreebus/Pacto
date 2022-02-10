a8fcb878021d619c726c9235d1d892da62a8184e
Kieran Woolley <kieran.woolley1@gmail.com>
2022-02-10 14:15:01 +0000

    Relocate POST login tests to their own file

---
+++ server/test/authRoutes/loginPOST.test.js
@@ -0,0 +1,118 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const EmailVerificationCode = require("../models/EmailVerificationCode");
+const supertest = require("supertest");
+const bcrypt = require("bcrypt");
+const app = require("../app");
+const Cookies = require("expect-cookies");
+const { createToken } = require("../controllers/authController");
+const University = require("../models/University");
+const User = require("../models/User");
+const { JsonWebTokenError } = require("jsonwebtoken");
+const { MESSAGES } = require("../helpers/messages");
+const { generateTestUser } = require('./fixtures/generateTestUser');
+
+// post login magic values
+const INCORRECT_CREDENTIALS = "Incorrect credentials.";
+const INACTIVE_ACCOUNT = "University email not yet verified.";
+
+describe("POST /login", () => {
+  beforeEach(async () => {
+    const user = await generateTestUser();
+    user.active = true;
+    await user.save();
+  });
+
+  async function isInvalidCredentials(uniEmail, password,	msg = INCORRECT_CREDENTIALS) {
+    const response = await supertest(app)
+      .post("/login")
+      .send({
+        uniEmail,
+        password,
+      })
+      .expect(400);
+
+    expect(response.body.message).toBe(null);
+    expect(response.body.errors[0].field).toBe(null);
+    expect(response.body.errors[0].message).toBe(msg);
+    expect(response.body.errors.length).toBe(1);
+  }
+
+  async function isValidCredentials(uniEmail, password) {
+    const response = await supertest(app)
+      .post("/login")
+      .send({
+        uniEmail,
+        password,
+      })
+      .expect(200)
+      .expect(
+        Cookies.set({
+          name: "jwt",
+          options: ["httponly"],
+        })
+      );
+    expect(response.body.message).toBeDefined();
+    expect(response.body.errors.length).toBe(0);
+  }
+
+  // Not using TEST_USER_EMAIL due to nature of these tests.
+  it("rejects invalid email", async () => {
+    await isInvalidCredentials("pac.to", "Password123");
+  });
+
+  it("rejects incorrect email", async () => {
+    await isInvalidCredentials("pac.to1@kcl.ac.uk", "Password123");
+  });
+
+  it("rejects invalid password", async () => {
+    await isInvalidCredentials("pac.to@kcl.ac.uk", "Password1");
+  });
+
+  it("rejects invalid email and password", async () => {
+    await isInvalidCredentials("pac.to1@kcl.ac.uk", "Password1");
+  });
+
+  it("logs the user in when the credentials are correct", async () => {
+    await isValidCredentials("pac.to@kcl.ac.uk", "Password123");
+  });
+
+  it("rejects inactive user with correct credentials", async () => {
+    await User.findOneAndUpdate(
+      { uniEmail: TEST_USER_EMAIL },
+      { active: false }
+    );
+    await isInvalidCredentials(
+      "pac.to@kcl.ac.uk",
+      "Password123",
+      INACTIVE_ACCOUNT
+    );
+  });
+});
+
+describe("GET /logout", () => {
+  beforeEach(async () => {
+    const user = await generateTestUser();
+    user.active = true;
+    await user.save();
+  });
+
+  it("clears the cookie even for non-logged in users", async () => {
+    const response = await supertest(app)
+      .get("/logout")
+      .expect(Cookies.set({name: "jwt", options: ["max-age"]}));
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("returns OK and cookie with max-age set when user logged in", async () => {
+    const user = await User.findOne({ uniEmail: TEST_USER_EMAIL });
+    const token = createToken(user._id);
+    const response = await supertest(app)
+      .get("/logout")
+      .set("Cookie", [`jwt=${token}`])
+      .expect(Cookies.set({name: "jwt", options: ["max-age"]}));
+
+    expect(response.statusCode).toBe(200);
+  });
+});
