b09bd0dcd10d549d34557ac852d42f3e60fe2e41
Kieran Woolley <kieran.woolley1@gmail.com>
2022-02-11 12:31:08 +0000

    Ensure all error messages in authMiddleware use messages.js helper

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -17,31 +17,22 @@ const checkAuthenticated = async (req, res, next) => {
       } 
       else {
 
-<<<<<<< HEAD
-        if(!user.active){
-          //throw Error("User has not verified their email");
-          res
-          .status(401)
-          .json(jsonResponse(null, [jsonError(null, MESSAGES.AUTH.IS_INACTIVE)]));
-        }
-=======
         try {
           let user = await User.findById(decodedToken.id);
           await user.populate({path: 'university', model: University});
           req.user = user;
->>>>>>> main
 
           if(!user.active){
             res
             .status(401)
-            .json(jsonResponse(null, [jsonError(null, "User has not verified their email")]));
+            .json(jsonResponse(null, [jsonError(null, MESSAGE.AUTH.IS_INACTIVE)]));
           } else {
             next();
           }
         }
         catch (err) {
           res.cookie("jwt", "", { maxAge: 1 });
-          res.status(401).json(jsonResponse(null, [jsonError(null, "You have to login")]))
+          res.status(401).json(jsonResponse(null, [jsonError(null, MESSAGE.AUTH.IS_NOT_LOGGED_IN)]));
         }
       }
     });
