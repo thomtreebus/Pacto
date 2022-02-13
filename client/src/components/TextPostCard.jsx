a180ddbe9a30774e53d7ebef8c518b5a978cfd88
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-02-13 22:00:29 +0000

    Rewrite TextPostCard to be more visually appealing
    Use Box's (div's) rather that Grid's as Grid layouts force a 12 column layout

--- client/src/components/TextPostCard.jsx
+++ client/src/components/TextPostCard.jsx
@@ -1,85 +1,87 @@
-import { Card, CardContent, IconButton } from "@mui/material";
+import { Box, Card, CardContent, IconButton } from "@mui/material";
 import { useState } from "react";
-import { Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 
 import ThumbUpRoundedIcon from "@mui/icons-material/ThumbUpRounded";
 import ThumbDownRoundedIcon from "@mui/icons-material/ThumbDownRounded";
 
 export default function TextPostCard({ post }) {
-  const [thumbUp, setThumbUp] = useState(upvoted);
-  const [thumbDown, setThumbDown] = useState(downvoted);
-  const [likes, setLikes] = useState(numberOfLikes);
+  const [thumbUp, setThumbUp] = useState(post.upvoted);
+  const [thumbDown, setThumbDown] = useState(post.downvoted);
+  const [likes, setLikes] = useState(post.votes);
 
   const handleLikeEvent = (event) => {
     // Dispatch message to server about like event
-    // 0 => Post was liked. May have previously been disliked.
-    // 1 => Post was unliked.
-    // 2 => Post was disliked. May have previously been liked.
+    // 0 => Post was unliked.
+    // 1 => Post was liked and undisliked.
+    // 2 => Post was liked.
     // 3 => Post was undisliked.
+    // 4 => Post was disliked and unliked.
+    // 5 => Post was disliked.
   }
 
   return (
     <Card sx={{ width: "100%" }}>
       <CardContent>
-        <Grid container direction="row">
-          <Grid container item direction="column" sm={2} lg={1} sx={{ alignSelf: "baseline", alignItems: "center" }}>
-            <Grid item>
-              <IconButton sx={{ paddingRight: 0, paddingLeft: 0, paddingTop: 0 ,paddingBottom: 0 }} onClick={() => {
-                if (!thumbUp) {
-                  setLikes(numberOfLikes + 1)
-                  setThumbDown(false);
-                  handleLikeEvent(0)
-                } else {
-                  setLikes(likes - 1)
-                  handleLikeEvent(1)
-                }
-                setThumbUp(!thumbUp);
-              }}>
-                <ThumbUpRoundedIcon color={thumbUp ? "primary" : "inherit"}/>
-              </IconButton>
-            </Grid>
+        <Box sx={{ overflow: "hidden" }}>
+          <Box sx={{ float: "left", paddingRight: "16px", textAlign: "center" }}>
+            <IconButton sx={{ paddingRight: 0, paddingLeft: 0, paddingTop: 0 ,paddingBottom: 0 }} onClick={() => {
+              if (thumbUp) {
+                setLikes(likes - 1);
+                handleLikeEvent(0);
+              } else if (thumbDown) {
+                setLikes(likes + 2);
+                setThumbDown(false);
+                handleLikeEvent(1);
+              } else {
+                setLikes(likes + 1)
+                handleLikeEvent(2);
+              }
+              setThumbUp(!thumbUp);
+            }}>
+              <ThumbUpRoundedIcon color={thumbUp ? "primary" : "inherit"}/>
+            </IconButton>
 
-            <Grid item paddingLeft={1}>
-              <Typography>
-                {post.likes}
-              </Typography>
-            </Grid>
+            <Typography>
+              {likes}
+            </Typography>
 
-            <Grid item>
-              <IconButton sx={{ paddingRight: 0, paddingLeft: 0, paddingTop: 0 ,paddingBottom: 0 }} onClick={() => {
-                if (!thumbDown) {
-                  setLikes(numberOfLikes - 1)
-                  handleLikeEvent(2)
-                  setThumbUp(false);
-                } else {
-                  setLikes(likes + 1)
-                  handleLikeEvent(3)
-                }
-                setThumbDown(!thumbDown);
-              }}>
-                <ThumbDownRoundedIcon color={thumbDown ? "secondary" : "inherit"}/>
-              </IconButton>
-            </Grid>
-          </Grid>
+            <IconButton sx={{ paddingRight: 0, paddingLeft: 0, paddingTop: 0 ,paddingBottom: 0 }} onClick={() => {
+              if (thumbDown) {
+                setLikes(likes + 1);
+                handleLikeEvent(3);
+              } else if (thumbUp) {
+                setLikes(likes - 2);
+                setThumbUp(false);
+                handleLikeEvent(4);
+              } else {
+                setLikes(likes - 1)
+                handleLikeEvent(5);
+              }
+              setThumbDown(!thumbDown);
+            }}>
+              <ThumbDownRoundedIcon color={thumbDown ? "secondary" : "inherit"}/>
+            </IconButton>
+          </Box>
 
-          <Grid container item direction="column" sm={10} lg={11} sx={{ cursor: "pointer" }}>
-            <Grid item>
-              <Typography variant="subtitle1">
-                Posted by {post.author} on {post.date}
-              </Typography>
-            </Grid>
-            <Grid item zeroMinWidth>
-              <Typography variant="h5">
-                {post.title}
-              </Typography>
-            </Grid>
-            <Grid item sx={{ float: "down" }}>
-              <Typography variant="subtitle2">
-                {post.numberOfComments} {post.numberOfComments === 1 ? "Comment" : "Comments"}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
+          <Box sx={{ overflow: "hidden" }}>
+            <Typography variant="caption">
+              Posted by {post.author.firstName + " " + post.author.lastName} on {post.date}
+            </Typography>
+
+            <Typography variant="h6">
+              {post.title}
+            </Typography>
+
+            <Typography variant="body1">
+              {post.text}
+            </Typography>
+
+            <Typography variant="subtitle2">
+              {post.comments.length} {post.comments.length === 1 ? "Comment" : "Comments"}
+            </Typography>
+          </Box>
+        </Box>
       </CardContent>
     </Card>
   )
