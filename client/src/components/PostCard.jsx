6ce470e8744f01fdb89462724e51b23e88f78184
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-02-13 21:59:39 +0000

    Fix minor issues in post component

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -3,18 +3,18 @@ import LinkPostCard from "./LinkPostCard";
 import TextPostCard from "./TextPostCard";
 
 export default function PostCard({ post }) {
-  concretePostCard = () => {
+  const ConcretePostCard = (() => {
     switch(post.type) {
       case "text": return TextPostCard;
       case "image": return ImagePostCard;
       case "link": return LinkPostCard;
       default: return;
     }
-  }
+  })()
 
   return (
-    <div className="post">
-      <concretePostCard post={post} />
+    <div className="post" style={{ minWidth: "100%" }}>
+      <ConcretePostCard post={post}/>
     </div>
   )
 }
