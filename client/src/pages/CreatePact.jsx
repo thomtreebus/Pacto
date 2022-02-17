f7837e86dd836c4b7c13792357d800721f7f2620
Aaron Patrick Monte <maaronpatrick@gmail.com>
2022-02-17 17:15:32 +0000

    Beautify create pact page

--- client/src/pages/CreatePact.jsx
+++ client/src/pages/CreatePact.jsx
@@ -1,11 +1,10 @@
 import * as React from "react";
+import { Card, Typography } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Icon from "../assets/pacto-logo.ico";
-import Typography from "@mui/material/Typography";
 import { useHistory } from "react-router-dom";
 import MenuItem from '@mui/material/MenuItem';
 
@@ -46,9 +45,9 @@ export default function LoginPage() {
 			}),
 		});
 
-		console.log(response);
 		const json = await response.json();
-		
+		console.log(json);
+
 		Object.values(json['errors']).forEach(err => {
 			const field = err["field"];
 			const message = err["message"];
@@ -67,8 +66,6 @@ export default function LoginPage() {
 		});
 
 		if (response.status !== 201) {
-			setIsButtonDisabled(true)
-			console.log(json);
 			return;
 		}
 
@@ -79,108 +76,112 @@ export default function LoginPage() {
 
 	return (
 		<>
+			<Card
+			sx={{
+				padding: "40px",
+				margin: "auto",
+			}}
+			>
+				<Box
+					sx={{
+						// height: "calc(100vh - 68.5px)",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						// backgroundColor: "white",
+						paddingInline: "200px",
+						// paddingBlock: "10px",
+						// marginTop: "68.5px" 
+					}}
+				>
+					<Avatar alt="Pacto Icon" src={Icon} />
+
+					<Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
+						Create Pact
+					</Typography>
 					<Box
-						sx={{
-							display: "flex",
-							flexDirection: "column",
-							alignItems: "center",
-              backgroundColor: "white",
-              paddingInline: "200px",
-              paddingBlock: "10px",
-              marginBlock: "auto" 
-						}}
+						component="form"
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 1 }}
 					>
-						<Avatar alt="Pacto Icon" src={Icon} />
-
-						<Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
-							Create Pact
-						</Typography>
-						<Box
-							component="form"
-							noValidate
-							onSubmit={handleSubmit}
-							sx={{ mt: 1 }}
-						>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								label="Pact Name"
-								name="pact-name"
-								error={apiPactNameError.length !== 0}
-                helperText={apiPactNameError}
-								autoFocus
-							/>
-
-            <TextField
-							data-testid='category-select'
-              alignitems='center'
-              margin="normal"
-              required
+						<TextField
+							margin="normal"
+							required
 							fullWidth
-              id="category-select"
-							name="category-select"
-              select
-              value={category}
-              onChange={handleCategoryChange}
-              label="Category"
-							error={apiPactCategoryError.length !== 0}
-              helperText={apiPactCategoryError}
-            >
-              <MenuItem value={"society"} data-testid="subject-item">
-                Society
-              </MenuItem>
-              <MenuItem value={"course"} data-testid="module-item">
-                Course
-              </MenuItem>
-              <MenuItem value={"module"} data-testid="society-item">
-                Module
-              </MenuItem>
-              <MenuItem value={"other"} data-testid="other-item">
-                Other
-              </MenuItem>
-            </TextField>
+							label="Pact Name"
+							name="pact-name"
+							error={apiPactNameError.length !== 0}
+							helperText={apiPactNameError}
+							autoFocus
+						/>
 
-            <TextField
-							name="description"
-              margin="normal"
-              id="description"
-              label="Description"
-              required
-              fullWidth
-              multiline
-              rows={4}
-							error={apiPactDescriptionError.length !== 0}
-              helperText={apiPactDescriptionError}
-            />
+					<TextField
+						data-testid='category-select'
+						alignitems='center'
+						margin="normal"
+						required
+						fullWidth
+						id="category-select"
+						name="category-select"
+						select
+						value={category}
+						onChange={handleCategoryChange}
+						label="Category"
+						error={apiPactCategoryError.length !== 0}
+						helperText={apiPactCategoryError}
+					>
+						<MenuItem value={"society"} data-testid="subject-item">
+							Society
+						</MenuItem>
+						<MenuItem value={"course"} data-testid="module-item">
+							Course
+						</MenuItem>
+						<MenuItem value={"module"} data-testid="society-item">
+							Module
+						</MenuItem>
+						<MenuItem value={"other"} data-testid="other-item">
+							Other
+						</MenuItem>
+					</TextField>
+
+					<TextField
+						name="description"
+						margin="normal"
+						id="description"
+						label="Description"
+						required
+						fullWidth
+						multiline
+						rows={4}
+						error={apiPactDescriptionError.length !== 0}
+						helperText={apiPactDescriptionError}
+					/>
+					<Button
+						fullWidth
+						label="Upload Image"
+						variant="contained"
+						component="span"
+						sx={{
+							marginTop: 1
+						}}
+						>
+						Upload Image
+					</Button>
+					
+						
 						<Button
-              fullWidth
-              label="Upload Image"
-              variant="contained"
-              component="span"
-              sx={{
-                marginTop: 1
-              }}
-              >
-              Upload Image
-            </Button>
-            
-							
-							<Button
-								type="submit"
-								fullWidth
-								variant="contained"
-								disabled={isButtonDisabled}
-								sx={{ mt: 3, mb: 2 }}
-							>
-								Create Pact
-							</Button>
-							<Grid container>
-							</Grid>
-						</Box>
+							type="submit"
+							fullWidth
+							variant="contained"
+							disabled={isButtonDisabled}
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Create Pact
+						</Button>
 					</Box>
-          <Grid container component="main" sx={{ height: "100%" }}>
-				</Grid>
+				</Box>
+			</Card>
 		</>
 	);
 }
