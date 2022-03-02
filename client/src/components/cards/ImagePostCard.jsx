e71478e0319c0f6c801f9385901bdaec67441f91
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-02 16:23:30 +0000

    Add a maximum height to a picture in a post

--- client/src/components/cards/ImagePostCard.jsx
+++ client/src/components/cards/ImagePostCard.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material"
 export default function ImagePostCard({ post }) {
   return (
     <Box textAlign="center">
-      <img src={post.image} style={{ "max-width": "100%"}}/>
+      <img src={post.image} style={{ maxWidth: "100%", maxHeight: "600px"}}/>
     </Box>
   )
 }
