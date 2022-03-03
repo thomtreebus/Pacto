1837362d6cdaf5486dee1f49bb5af0562eada9e8
Kieran Woolley <kieran.woolley1@gmail.com>
2022-03-03 14:35:11 +0000

    Ensure comment routes use checkValidPost middleware

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const commentController = require("../controllers/commentController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 const { checkIsMemberOfPact } = require("../middleware/pactMiddleware");
+const { checkValidPost } = require("../middleware/postMiddleware");
 
-router.post("/pact/:pactId/post/:postId/comment", checkAuthenticated, checkIsMemberOfPact, commentController.commentPost);
+router.post("/pact/:pactId/post/:postId/comment", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.commentPost);
 
-router.delete("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, commentController.commentDelete);
-router.get("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, commentController.commentGet);
+router.delete("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.commentDelete);
+router.get("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.commentGet);
 
-router.put("/pact/:pactId/post/:postId/comment/:commentId/upvote", checkAuthenticated, checkIsMemberOfPact, commentController.commentUpvotePut);
-router.put("/pact/:pactId/post/:postId/comment/:commentId/downvote", checkAuthenticated, checkIsMemberOfPact, commentController.commentDownvotePut);
+router.put("/pact/:pactId/post/:postId/comment/:commentId/upvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.commentUpvotePut);
+router.put("/pact/:pactId/post/:postId/comment/:commentId/downvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.commentDownvotePut);
 
 module.exports = router;
