f57cd9b42b0d0c3556e484a70973320a72f802a4
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-04 00:14:22 +0000

    Add blank alt to image to silence warning

--- client/src/components/cards/ImagePostCard.jsx
+++ client/src/components/cards/ImagePostCard.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material"
 export default function ImagePostCard({ post }) {
   return (
     <Box textAlign="center">
-      <img src={post.image} style={{ maxWidth: "100%", maxHeight: "600px"}}/>
+      <img src={post.image} alt="" style={{ maxWidth: "100%", maxHeight: "600px"}}/>
     </Box>
   )
 }
