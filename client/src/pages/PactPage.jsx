4853c09d6e373b232d134dfccbf1394eaaf4258d
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-04 00:10:06 +0000

    Hookup pact page with backend
    Posts are still hard coded for now, and will be the same for all pacts. This is because the seeder doesn't seed any posts yet, and also some more work is required on the backend to filter what data is sent

--- client/src/pages/PactPage.jsx
+++ client/src/pages/PactPage.jsx
@@ -164,40 +164,42 @@ const testdata = [
 
 export default function PactPage() {
   const { pactID } = useParams();
-  const [posts, setPosts] = useState(true);
+  const [pact, setPact] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_URL}/pact/${pactID}/posts`, {
+    fetch(`${process.env.REACT_APP_URL}/pact/${pactID}`, {
       method: "GET",
       credentials: "include"
     }).then((res) => {
       if (!res.ok) {
-        throw Error("Could not fetch posts");
+        throw Error("Could not fetch pact");
       }
       return res.json();
     }).then((data) => {
-      setPosts(data.message);
+      setPact(data.message);
       setIsLoading(false);
       setError(null);
     }).catch((err) => {
-      setError(err);
+      console.log(err);
+      setPact(null);
       setIsLoading(false);
+      setError(err);
     })
   }, [pactID])
 
   return (
     <>
-    {/* { isLoading && <Loading /> }
-    { error && <ErrorPage /> } */}
+    { isLoading && <Loading /> }
+    { error && <ErrorPage /> }
       <Grid container width={"50vw"}>
         <Grid item xs={12} lg={8}>
-          { posts && <PostList posts={testdata}/> }
+          { pact && <PostList posts={testdata}/> }
         </Grid>
         <Grid item lg={4}>
           <Box sx={{ paddingTop: "16px", paddingRight: "16px" }} display={{ xs: "none", lg: "block" }} position={"sticky"} top={65}>
-            { posts && <AboutPact pact={{ description: "Hello I am Pact", posts: [0,0,0,0,0,0,0,0], members: [0,0,0,0,0,0], name: "The Coolest Pact" }} /> }
+            { pact && <AboutPact pact={pact} /> }
           </Box>
         </Grid>
       </Grid>
