b5ad3713aafbc6679a97e64331e5a472b67498a0
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-08 15:33:34 +0000

    Set up acceptFriendRequest tests

---
+++ server/test/userRoutes/acceptFriendRequest.test.js
@@ -0,0 +1,35 @@
+const User = require("../../models/User");
+const FriendRequest = require("../../models/FriendRequest");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("../../app");
+const supertest = require("supertest");
+const { generateTestUser, getDefaultTestUserEmail, generateCustomUniTestUser} = require("../fixtures/generateTestUser");
+const { createToken } = require("../../controllers/authController");
+const { MESSAGES, FRIEND_REQUEST_MESSAGES } = require("../../helpers/messages");
+
+dotenv.config();
+
+describe("acceptFriendRequest /friends", () => {
+  beforeAll(async () => {
+  await mongoose.connect(process.env.TEST_DB_CONNECTION_URL);
+	});
+
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+
+  beforeEach(async () => {
+    const user = await generateTestUser();
+    user.active = true;
+    await user.save();
+    const recipientUser = await generateCustomUniTestUser("pacTwo");
+    recipientUser.active = true;
+    await recipientUser.save();
+  });
+
+	afterEach(async () => {
+		await User.deleteMany({});
+	});
+
+});
