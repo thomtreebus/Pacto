0ec9acb884a8f62e20358c87e39b109d933b4fdb
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-08 15:26:51 +0000

    Write tests for sendFriendRequest

--- server/test/userRoutes/sendFriendRequest.test.js
+++ server/test/userRoutes/sendFriendRequest.test.js
@@ -1,14 +1,14 @@
 const User = require("../../models/User");
+const FriendRequest = require("../../models/FriendRequest");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("../../app");
 const supertest = require("supertest");
 const { generateTestUser, getDefaultTestUserEmail, generateCustomUniTestUser} = require("../fixtures/generateTestUser");
 const { createToken } = require("../../controllers/authController");
-const { MESSAGES, FRIEND_REQUEST_MESSAGES } = require("../../helpers/messages");
+const { MESSAGES, FRIEND_MESSAGES } = require("../../helpers/messages");
 
 dotenv.config();
-
 
 describe("sendFriendRequest /friends", () => {
   beforeAll(async () => {
@@ -40,7 +40,65 @@ describe("sendFriendRequest /friends", () => {
     .post(`/friends/${ recipientUser._id }`)
     .set("Cookie", [`jwt=${ token }`])
     .expect(201)
+
     expect(response.body.message).toBeDefined();
     expect(response.body.errors.length).toBe(0);
-  })
+
+    const updatedUser = await User.findOne({ uniEmail: getDefaultTestUserEmail() });
+    const updatedRecipient = await User.findOne({_id: recipientUser._id})
+    expect(updatedUser.sentRequests.length).toBe(1);
+    expect(updatedRecipient.receivedRequests.length).toBe(1);
+  });
+
+  it("does not let user send a friend request to existing friend", async () => {
+    const recipientUser = await generateCustomUniTestUser("User", "ucl");
+    const user = await User.findOne({ uniEmail: getDefaultTestUserEmail() });
+    const friends = [user._id];
+    await User.findByIdAndUpdate(recipientUser._id, {friends});
+    
+    const token = createToken(user._id);
+    const response = await supertest(app)
+    .post(`/friends/${ recipientUser._id }`)
+    .set("Cookie", [`jwt=${ token }`])
+    .expect(400)
+
+    expect(response.body.message).toBeDefined();
+    expect(response.body.errors[0].field).toBe(null);
+    expect(response.body.errors.length).toBe(1);
+    expect(response.body.errors[0].message).toBe(FRIEND_MESSAGES.ALREADY_FRIEND);
+  });
+
+  it("does not let user send a friend request if request has already been sent", async () => {
+    const recipientUser = await generateCustomUniTestUser("User", "ucl");
+    const user = await User.findOne({ uniEmail: getDefaultTestUserEmail() });
+    const token = createToken(user._id);
+    
+    const request = await FriendRequest.create({requestor: user, recipient: recipientUser});
+    const sentRequests = [request._id];
+    await User.findByIdAndUpdate(recipientUser._id, {sentRequests});
+    
+    await supertest(app)
+    .post(`/friends/${ recipientUser._id }`)
+    .set("Cookie", [`jwt=${ token }`])
+    .expect(400)
+
+    expect(response.body.message).toBeDefined();
+    expect(response.body.errors.length).toBe(1);
+    expect(response.body.errors[0].field).toBe(null);
+    expect(response.body.errors[0].message).toBe(FRIEND_MESSAGES.ALREADY_SENT);
+  });
+
+  it("check uses authMiddleware", async () => {
+    const recipientUser = await generateCustomUniTestUser("User", "ucl");
+    const user = await User.findOne({ uniEmail: getDefaultTestUserEmail() });
+
+    const response = await supertest(app)
+    .post(`/friends/${ recipientUser._id }`)
+
+    expect(response.body.message).toBe(null);
+    expect(response.body.errors[0].field).toBe(null);
+    expect(response.body.errors[0].message).toBe(MESSAGES.AUTH.IS_NOT_LOGGED_IN);
+    expect(response.body.errors.length).toBe(1);
+  });
+
 });
