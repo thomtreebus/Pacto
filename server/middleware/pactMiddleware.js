36f1404d77508a43d0b9ce6cc921c18cf3f73851
Thom Treebus <91496572+thom-treebus@users.noreply.github.com>
2022-03-10 15:11:41 +0000

    Update pactMiddleware.js

--- server/middleware/pactMiddleware.js
+++ server/middleware/pactMiddleware.js
@@ -46,22 +46,7 @@ const checkIsModeratorOfPact = async (req, res, next) => {
   let status = 400;
   try {
     const user = req.user;
-    if (!user){
-      status = 401;
-      throw Error(MESSAGES.AUTH.IS_NOT_LOGGED_IN);
-    }
-
-    let pact = null;
-		try {
-			pact = await Pact.findOne({ university: user.university, _id:req.params.pactId });
-		}
-		catch (err) {
-			pact = null;
-		}
-    if (!pact) {
-      status = 404;
-      throw Error(PACT_MESSAGES.NOT_FOUND);
-    }
+    const pact = await Pact.findOne({ university: user.university, _id:req.params.pactId });
 
     const id = user._id.toString();
 
