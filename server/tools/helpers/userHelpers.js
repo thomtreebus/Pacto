52bc2df339e75689dd206b6e101677d2e83ce74b
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-10 18:04:20 +0000

    Extract user seeder into a helper file

---
+++ server/tools/helpers/userHelpers.js
@@ -0,0 +1,62 @@
+const Chance = require("chance");
+const bcrypt = require("bcrypt");
+const userConstants = require("./userConstants");
+const User = require("../../models/User");
+
+const chance = new Chance();
+const SALT_ROUNDS = 10;
+
+async function seedUsers(university, USER_COUNT) {
+	const names = chance.unique(chance.name, USER_COUNT);
+
+	for (let i = 0; i < names.length; i++) {
+		await createUser(names[i].split(" ")[0], names[i].split(" ")[1], university);
+	}
+
+	await createUser("pac", "to", university);
+	console.log(`Finished seeding ${USER_COUNT} users`);
+}
+
+async function createUser(firstName, lastName, university) {
+	const salt = await bcrypt.genSalt(SALT_ROUNDS);
+	const hashedPassword = await bcrypt.hash("Password123", salt);
+
+	const user = await User.create({
+		firstName,
+		lastName,
+		uniEmail: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@kcl.ac.uk`,
+		password: hashedPassword,
+		active: true,
+		university: university,
+		course: getRandomCourse(),
+		location: getRandomLocation(),
+		image: getRandomImage(firstName, lastName),
+		bio: chance.paragraph(),
+		hobbies: getRandomHobbies(),
+		friends: [],
+	});
+
+	university.users.push(user);
+	await university.save();
+}
+
+function getRandomImage(firstName, lastName) {
+	return `https://avatars.dicebear.com/api/personas/${firstName.toLowerCase()}${lastName.toLowerCase()}.svg`;
+}
+
+function getRandomCourse() {
+	return userConstants.COURSES[chance.integer({ min: 0, max: userConstants.COURSES.length })];
+}
+
+function getRandomLocation() {
+	return userConstants.CITIES[chance.integer({ min: 0, max: userConstants.CITIES.length })]
+}
+
+function getRandomHobbies() {
+	const hobbies = []; 
+	randomHobby = () => chance.integer({ min: 0, max: userConstants.HOBBIES.length })
+	chance.unique(randomHobby, 2).forEach(hobby => hobbies.push(userConstants.HOBBIES[hobby]));
+	return hobbies;
+}
+
+module.exports.seedUsers = seedUsers;
