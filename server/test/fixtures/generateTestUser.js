289d90e75052d37b2572c5c751512b9c29ffa4e4
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-10 20:54:01 +0000

    Add comments to generateTestUser.js

--- server/test/fixtures/generateTestUser.js
+++ server/test/fixtures/generateTestUser.js
@@ -5,15 +5,18 @@ const {getDefaultTestUser} = require("../helpers/defaultTestUser");
 
 const SALT_ROUNDS = 10;
 
+// Returns the default test user email of when generateTestUser is called without parameters.
 module.exports.getDefaultTestUserEmail = () => {
 	const defaultUser = getDefaultTestUser();
 	return defaultUser.uniEmail;
 }
 
+// Generates a test user. If not parameters are specified it uses default values.
 module.exports.generateTestUser = async (name = "pac") => {
 	return generateCustomUniTestUser(name, "kcl");
 };
 
+// Generate test user with a specified uni as a uniName String
 const generateCustomUniTestUser = async (name, uniName = "kcl") => {
 	// Dummy uni
 	let uni = await University.findOne( { name: uniName, domains: [`${ uniName }.ac.uk`] });
