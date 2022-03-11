385d857e6b09a1b3eb4eeda2bbac8b8ff36dd889
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-11 09:56:18 +0000

    Create controller for search bar functionality

--- server/controllers/universityController.js
+++ server/controllers/universityController.js
@@ -14,3 +14,38 @@ module.exports.universityGet = async (req, res) => {
     res.status(400).json(jsonResponse(null, [jsonError(null, err.message)]));
   }
 }
+
+module.exports.search = async (req, res) => {
+  try {
+    const searchQuery = req.params.query;
+    const university = await University.findOne({ id: req.user.university });
+
+    // Find all pacts matching the query string
+    const pacts = Pact.find({
+      university: university._id,
+      name: { $regex: searchQuery }
+    });
+
+    // Find all users who's name match the query string
+    const users = User.find({
+      university: university._id,
+      name: { $regex: searchQuery }
+    });
+
+    // Find all posts matching the query string
+    const uniPacts = Pact.find({ university: university._id });
+    let posts = [];
+    for (let pact in uniPacts) {
+      posts.concat(Post.find({ pact: pact._id, title: { $regex: searchQuery } }));
+    }
+    const results = {
+      pacts: pacts,
+      users: users,
+      posts: posts
+    }
+    res.status(200).json(jsonResponse(uni, []));
+  } 
+  catch (err) {
+    res.status(400).json(jsonResponse(null, [jsonError(null, err.message)]));
+  }
+}
