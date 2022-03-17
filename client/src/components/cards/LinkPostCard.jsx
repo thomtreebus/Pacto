e992fa81997a97f4e15a28b98c4f275890404bdd
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-17 23:25:16 +0000

    Implement link previews

--- client/src/components/cards/LinkPostCard.jsx
+++ client/src/components/cards/LinkPostCard.jsx
@@ -1,5 +1,51 @@
+import {Card, CardActionArea, CardContent, CardMedia, Typography, Link } from "@mui/material";
+import { useEffect, useState } from "react";
+
 export default function LinkPostCard({ post }) {
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_LINKPREVIEW_URL}/?key=${process.env.REACT_APP_LINKPREVIEW_KEY}&q=${post.link}`, {
+      method: "GET",
+    }).then((res) => {
+      console.log(res);
+      if (!res.ok) {
+        throw Error("Could not fetch pact");
+      }
+      return res.json();
+    }).then((data) => {
+      setImage(data.image);
+      setText(data.title);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, [post])
+
   return (
-    <a href={post.link}>{post.link}</a>
+    <Card sx={{ marginBottom: "5px" }}>
+      <Link href={post.link} sx={{ textDecoration: "none", color: "black" }}>
+        <CardActionArea>
+          {image &&
+          <CardMedia
+            component="img"
+            height="140"
+            image={image}
+          />}
+          {text &&
+          <CardContent sx={{ backgroundColor: "gold" }}>
+            <Typography>
+              {text}
+            </Typography>
+          </CardContent>
+          }
+          <CardContent sx={{ padding: "5px", backgroundColor: "gray", color: "white" }}>
+            <Typography variant={"subtitle"}>
+              {post.link}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+    </Card>
   )
 }
