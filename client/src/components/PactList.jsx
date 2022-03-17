d7cf0283a92630cdbcdfe78b995e6dc55fcb2d5d
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-17 10:48:50 +0000

    Extract pact list item into seperate component

--- client/src/components/PactList.jsx
+++ client/src/components/PactList.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
-import { useHistory } from "react-router-dom";
 import { Typography } from "@mui/material";
+import PactListItem from "./PactListItem";
 
 export default function PactList({ pacts }) {
-	const history = useHistory();
-
 	if (!pacts.length) {
 		return (
 			<Typography variant="subtitle1" sx={{ textAlign: "center" }}>
@@ -20,16 +14,7 @@ export default function PactList({ pacts }) {
 	return (
 		<div>
 			{pacts.map((pact) => (
-				<ListItem
-					key={pact._id}
-					onClick={(event) => history.replace(`/pact/${pact._id}`)}
-					sx={{ cursor: "pointer" }}
-				>
-					<ListItemIcon>
-						<Avatar src={pact.image} />
-					</ListItemIcon>
-					<ListItemText primary={pact.name} />
-				</ListItem>
+				<PactListItem pact={pact} key={pact._id} />
 			))}
 		</div>
 	);
