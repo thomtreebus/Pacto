998305185d77720f13ba820abb7edefd197ffa8e
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-17 22:29:34 +0000

    Optimise imports

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,28 +1,12 @@
-import { Box, Typography, Card, Fab } from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import Tab from "@mui/material/Tab";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import React, { useState } from "react";
-import background from "../assets/hub-background.jpg";
-import { Divider } from "@mui/material";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
-import AddIcon from "@mui/icons-material/Add";
-import { useHistory } from "react-router-dom";
-import PactGrid from "../components/PactGrid";
-import { useQuery } from "react-query";
-import { useAuth } from '../providers/AuthProvider';
+import React, {useEffect, useState} from "react";
+import {useHistory} from "react-router-dom";
+import {useAuth} from '../providers/AuthProvider';
 import Loading from "./Loading";
-import { useEffect } from "react";
-import UserPortfolio from "../components/TabComponent/UserPortfolio";
-import UserCard from "../components/UserCard";
 import UserList from "../components/UserList";
 import Tabs from "@mui/material/Tabs";
-import MyPactList from "../components/MyPactList";
 import PropTypes from "prop-types";
-
 
 
 function TabPanel(props) {
