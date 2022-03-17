73ad0e235b669e3c604bf816348a679088653300
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-17 10:00:23 +0000

    Add check in pact list to see if there are no pacts

--- client/src/components/PactList.jsx
+++ client/src/components/PactList.jsx
@@ -4,9 +4,18 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Avatar from "@mui/material/Avatar";
 import { useHistory } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 export default function PactList({ pacts }) {
 	const history = useHistory();
+
+	if (!pacts.length) {
+		return (
+			<Typography variant="subtitle1" sx={{ textAlign: "center" }}>
+				You are not in any pacts
+			</Typography>
+		);
+	}
 
 	return (
 		<div>
@@ -14,6 +23,7 @@ export default function PactList({ pacts }) {
 				<ListItem
 					key={pact._id}
 					onClick={(event) => history.replace(`/pact/${pact._id}`)}
+					sx={{ cursor: "pointer" }}
 				>
 					<ListItemIcon>
 						<Avatar src={pact.image} />
