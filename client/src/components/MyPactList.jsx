5cf6ef7758d9c6d8ffbc0b804f47a372426d90d3
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-17 09:59:51 +0000

    Extract my pact list logic into seperate component

---
+++ client/src/components/MyPactList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import List from "@mui/material/List";
+import { useAuth } from "../providers/AuthProvider";
+import { useQuery } from "react-query";
+import PactList from "./PactList";
+import Loading from "../pages/Loading";
+
+export default function MyPactList() {
+	const { isLoading, data } = useQuery("mypacts", () =>
+		fetch(`${process.env.REACT_APP_URL}/university`, {
+			credentials: "include",
+		}).then((res) => res.json())
+	);
+
+	const { user } = useAuth();
+
+	if (isLoading) {
+		return <Loading />;
+	}
+
+	return (
+		<List>
+			<PactList
+				pacts={data.message.pacts.filter((pact) =>
+					user.pacts.includes(pact._id)
+				)}
+			/>
+		</List>
+	);
+}
