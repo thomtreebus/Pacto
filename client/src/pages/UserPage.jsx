9aecac5413abaaeddffed71fa67440bee766b286
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-17 21:53:15 +0000

    Use endpoint for /users
    - Add the endpoint to fetch all users

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -23,9 +23,35 @@ import UserList from "../components/UserList";
 export default function UserPage() {
 
     const { user } = useAuth();
+    const [isLoading, setIsLoading] = useState(true);
+    const [allUsers, setAllUsers] = useState(null);
+    const history = useHistory();
+
+    useEffect(() => {
+    fetch(`${process.env.REACT_APP_URL}/users`, {
+      method: "GET",
+      credentials: "include"
+    }).then((res) => {
+      if (!res.ok) {
+        throw Error("Could not fetch pact");
+      }
+      return res.json();
+    }).then((data) => {
+      setAllUsers(data.message);
+      setIsLoading(false);
+    }).catch(() => {
+      history.push("/not-found");
+    })
+  }, [])
+
+  if(isLoading){
+    return (
+      <Loading/>
+    )
+  }
 	
 	return (
         //<UserPortfolio user = {user}></UserPortfolio>
-        <UserList users={[user, user, user, user]}></UserList>
+        <UserList users={allUsers}></UserList>
     )
 }
