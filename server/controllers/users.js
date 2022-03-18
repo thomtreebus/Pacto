077ee2c34fc9d36c9e0cbc50a94038954f1795d4
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-18 16:18:36 +0000

    Revert changes to demo
    - Restore changes to user.js
    - Update changes from post validation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -21,27 +21,20 @@ module.exports.updateProfile = async(req, res) => {
     } else if (req.user._id.toString() !== id.toString()) {
       status = 401;
       throw Error(USER_MESSAGES.UPDATE_OTHER_PROFILE_UNAUTHORISED)
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(id, { ...req.body }, {runValidators: true});
+      status = 200
+      const university = req.user.university;
+      resMessage = await User.findOne({university, _id: req.params.id}).populate(
+        {path: 'university', model: University}
+      );
     }
-    const updatedUser = await User.findByIdAndUpdate(id, { ...req.body }, {runValidators: true});
-    status = 200
-
-    const university = req.user.university;
-    resMessage = await User.findOne({university, _id: req.params.id}).populate(
-      {path: 'university', model: University}
-    );
   } catch (err) {
     // When status code is not defined use status 500
     if(!status){
       status = 500;
     }
-    // converts error array into json array.
-    const fieldErrors = errorHandler(err);
-    if(fieldErrors.length !== 0){
-      fieldErrors.forEach((myErr) => jsonErrors.push(myErr));
-    }
-    else {
-      jsonErrors.push(jsonError(null, err.message));
-    }
+    jsonErrors = [jsonError(null, err.message)];
   }
   finally {
     res.status(status).json(jsonResponse(resMessage, jsonErrors));
