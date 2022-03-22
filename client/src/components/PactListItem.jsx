8516e90cb59e7c05120a8527be691c6b76f192a2
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-22 22:32:46 +0000

    Remove duplicate keys

--- client/src/components/PactListItem.jsx
+++ client/src/components/PactListItem.jsx
@@ -73,7 +73,6 @@ export default function PactListItem({ pact }) {
 							top: "50%",
 							left: "50%",
 							transform: "translate(-50%, -50%)",
-							width: 400,
 							bgcolor: "background.paper",
 							boxShadow: 24,
 							pt: 2,
