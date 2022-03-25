3eed98f9903c2337dc4f1a4eb60d72121186eb8c
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-25 00:51:02 +0000

    Fix link previews on feed page

--- server/controllers/feedController.js
+++ server/controllers/feedController.js
@@ -2,6 +2,7 @@ const Post = require("../models/Post");
 const Pact = require("../models/Pact");
 const {jsonResponse, jsonError} = require("../helpers/responseHandlers");
 const User = require("../models/User");
+const getPreview = require("../helpers/LinkCache")
 
 module.exports.feedGET = async (req, res) => {
   try {
@@ -9,7 +10,17 @@ module.exports.feedGET = async (req, res) => {
     await user.populate({ path: 'pacts', model: Pact })
     const posts = await Post.find({ pact: { $in: user.pacts } }).sort({ createdAt: -1 });
     for(let i = 0; i < posts.length; i++) {
-      await posts[i].populate({ path: 'author', model: User });
+      const post = posts[i];
+      await post.populate({ path: 'author', model: User });
+      if (post.type === "link") {
+        console.log("getting preview...")
+				const preview = await getPreview(post.link);
+        console.log(preview)
+				if (preview !== null) {
+					post.text = preview.text;
+					post.image = preview.image;
+				}
+			}
     }
     res.status(200).json(jsonResponse(posts, []));
   } catch (err) {
