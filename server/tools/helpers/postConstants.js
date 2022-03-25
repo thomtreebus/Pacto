0c7d7a7bf8caff5d32ffc140132cca759362aff0
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-25 12:09:54 +0000

    Add more links to seeder for posts

--- server/tools/helpers/postConstants.js
+++ server/tools/helpers/postConstants.js
@@ -1,7 +1,16 @@
 module.exports.LINKS = [
 	"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-	"https://www.youtube.com/watch?v=xnZjtR9z1qA",
-	"https://twitter.com/kyodo_english/status/1504169867250847744",
 	"https://www.reddit.com/r/AskUK/comments/tfdu70/where_would_you_live_in_the_uk_if_you_are_fully/",
 	"https://www.reddit.com/r/Cricket/comments/tfh14i/match_thread_2nd_test_west_indies_vs_england_day_1/",
+	"https://www.pennlive.com/life/2022/03/doritos-bags-will-now-have-5-fewer-chips-thanks-to-inflation.html",
+	"https://blogs.kcl.ac.uk/proged/2007/01/26/microsoft-patents-bluej/",
+	"https://www.goodto.com/food/food-news/tesco-meal-deal-price-upped-3-50-652307",
+	"https://www.bbc.co.uk/newsround/60144487",
+	"https://www.insider.com/facebook-gen-z-teens-boomer-social-network-leaks-2021-10",
+	"https://www.kristv.com/news/america-in-crisis/petition-underway-to-rename-columbus-ohio-flavortown-as-city-removes-christopher-columbus-statue",
+	"https://www.insider.com/gorillas-afraid-of-shaq-miami-zoo-ron-magill-2022-2",
+	"https://www.nbcnews.com/news/us-news/man-who-ate-120-000-art-banana-said-he-would-n1098391",
+	"https://www.foxnews.com/food-drink/mcdonalds-robber-chicken-nuggets-breakfast-food-because-it-was-still-too-early",
+	"https://thehill.com/changing-america/well-being/prevention-cures/505499-ohio-ice-cream-shop-asks-customers-to-stop",
+	"https://www.youtube.com/watch?v=L_b277qj3P4"
 ];
