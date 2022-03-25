5a5a154c5e03efafdba2956af09a154c9e95ba72
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-25 10:35:44 +0000

    Add back getNotification route + tests

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -3,6 +3,27 @@ const {jsonResponse, jsonError} = require("../helpers/responseHandlers");
 const Notification = require("../models/Notification");
 const { NOTIFICATION_MESSAGES } = require("../helpers/messages");
 
+module.exports.getNotifications = async (req, res) => {
+  let notifications = null;
+  try {
+    notifications = await Notification.find({ user: req.user._id });
+    try {
+      for (let i = 0; i < notifications.length; i++) {
+        const notification = notifications[i];
+        await notification.populate({ path: 'user', model: User});
+      }
+      
+			res.status(200).json(jsonResponse(notifications, []));
+		} 
+		catch (err) {
+			res.status(400).json(jsonResponse(null, [jsonError(null, err.message)]));
+		}
+	}
+	catch (err) {
+		res.status(404).json(jsonResponse(null, [jsonError(null, NOTIFICATION_MESSAGES.NOT_FOUND)]));
+	}
+}
+
 module.exports.markAsRead = async (req, res) => {
 	try {
 		const notification = await Notification.findOne({ _id: req.params.id });
--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getNotifications, markAsRead } = require('../controllers/notificationController');
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 
-// router.get("/notifications", checkAuthenticated, getNotifications);
+router.get("/notifications", checkAuthenticated, getNotifications);
 
 router.put("/notifications/:id/update", checkAuthenticated, markAsRead);
 
