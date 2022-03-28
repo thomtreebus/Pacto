659df2e32010a8082ccf2dd65a7223ff1ffd33e4
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-03-28 12:58:15 +0100

    Update Friends button tests

--- client/src/tests/FriendButtons.test.js
+++ client/src/tests/FriendButtons.test.js
@@ -4,68 +4,77 @@ import "@testing-library/jest-dom";
 import MockComponent from "./utils/MockComponent";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import FriendButtons from "../components/FriendButtons"
-import testUsers from "./utils/testUsers"
+import FriendButtons from "../components/FriendButtons";
+import testUsers from "./utils/testUsers";
 
 const loggedInUser = {
-  firstName: "pac", 
-  lastName: "to",
-  image: "https://avatars.dicebear.com/api/identicon/temp.svg",
-  friends: [1], // Logged in user is friends with testUsers[0]
-  sentRequests: [{_id: 1, recipient: 2}], // and has sent a request to testUser[1]
-  receivedRequests: [{_id: 2, requestor: 3}], // and has received a request from testUser[2]
-  // User has no relationship with testUseer[3]
-}
+	firstName: "pac",
+	lastName: "to",
+	image: "https://avatars.dicebear.com/api/identicon/temp.svg",
+	friends: [1], // Logged in user is friends with testUsers[0]
+	sentRequests: [{ _id: 1, recipient: 2 }], // and has sent a request to testUser[1]
+	receivedRequests: [{ _id: 2, requestor: 3 }], // and has received a request from testUser[2]
+	// User has no relationship with testUseer[3]
+};
 
 describe("FriendButton Tests", () => {
 	const server = setupServer(
-    rest.get(`${process.env.REACT_APP_URL}/me`, (req, res, ctx) => {
+		rest.get(`${process.env.REACT_APP_URL}/me`, (req, res, ctx) => {
 			return res(
-				ctx.json({ 
-          message: {loggedInUser}, 
-          errors: [] 
-        })
-			)
+				ctx.json({
+					message: loggedInUser,
+					errors: [],
+				})
+			);
 		}),
 
-    rest.post(`${process.env.REACT_APP_URL}/friends/:4`, (req, res, ctx) => {
+		rest.post(`${process.env.REACT_APP_URL}/friends/:4`, (req, res, ctx) => {
 			return res(
-				ctx.json({ 
-          message: {}, 
-          errors: [] 
-        })
-			)
-		}), 
-
-    rest.put(`${process.env.REACT_APP_URL}/friends/:2/accept`, (req, res, ctx) => {
-			return res(
-				ctx.json({ 
-          message: {}, 
-          errors: [] 
-        })
+				ctx.json({
+					message: {},
+					errors: [],
+				})
 			);
-		}), 
+		}),
 
-    rest.put(`${process.env.REACT_APP_URL}/friends/:2/reject`, (req, res, ctx) => {
-			return res(
-				ctx.json({ 
-          message: {}, 
-          errors: [] 
-        })
-      );
-		}), 
+		rest.put(
+			`${process.env.REACT_APP_URL}/friends/:2/accept`,
+			(req, res, ctx) => {
+				return res(
+					ctx.json({
+						message: {},
+						errors: [],
+					})
+				);
+			}
+		),
 
-    rest.put(`${process.env.REACT_APP_URL}/friends/remove/:1`, (req, res, ctx) => {
-			return res(
-				ctx.json({ 
-          message: {}, 
-          errors: [] 
-        })
-			);
-		}), 
+		rest.put(
+			`${process.env.REACT_APP_URL}/friends/:2/reject`,
+			(req, res, ctx) => {
+				return res(
+					ctx.json({
+						message: {},
+						errors: [],
+					})
+				);
+			}
+		),
+
+		rest.put(
+			`${process.env.REACT_APP_URL}/friends/remove/:1`,
+			(req, res, ctx) => {
+				return res(
+					ctx.json({
+						message: {},
+						errors: [],
+					})
+				);
+			}
+		)
 	);
 
-  beforeAll(() => {
+	beforeAll(() => {
 		server.listen();
 	});
 
@@ -82,94 +91,94 @@ describe("FriendButton Tests", () => {
 		await waitForElementToBeRemoved(() => screen.getByText("Loading"));
 	};
 
-    describe("Check element rendering and interaction: is-friend case", () => {
-        beforeEach(async () => {
-          await renderWithMock(<FriendButtons currentUser={loggedInUser} user={testUsers[0]} />);
-        });
+	describe("Check element rendering and interaction: is-friend case", () => {
+		beforeEach(async () => {
+			await renderWithMock(
+				<FriendButtons currentUser={loggedInUser} user={testUsers[0]} />
+			);
+		});
 
-        it("should render delete friend button", async () => {
-            const delFriendBtn = await screen.findByTestId("del-friend-btn");
-            expect(delFriendBtn).toBeInTheDocument();
-        });
+		it("should render delete friend button", async () => {
+			const delFriendBtn = await screen.findByTestId("del-friend-btn");
+			expect(delFriendBtn).toBeInTheDocument();
+		});
 
-        it("handles delete friend press", async () => {
-            const delFriendBtn = await screen.findByTestId("del-friend-btn");
-            fireEvent.click(delFriendBtn);
+		it("handles delete friend press", async () => {
+			const delFriendBtn = await screen.findByTestId("del-friend-btn");
+			fireEvent.click(delFriendBtn);
+			const addFriendBtn = await screen.findByTestId("add-friend-btn");
+			expect(addFriendBtn).toBeInTheDocument();
+			expect(delFriendBtn).not.toBeInTheDocument();
+		});
+	});
 
-            expect(delFriendBtn).not.toBeInTheDocument();
+	describe("Check element rendering and interaction: request-sent case", () => {
+		beforeEach(async () => {
+			await renderWithMock(
+				<FriendButtons currentUser={loggedInUser} user={testUsers[1]} />
+			);
+		});
 
-            const addFriendBtn = await screen.findByTestId("add-friend-btn");
-            expect(addFriendBtn).toBeInTheDocument();
-        });
-    });
+		it("should render request sent button", async () => {
+			const reqSentBtn = await screen.findByTestId("sent-req-btn");
+			expect(reqSentBtn).toBeInTheDocument();
+			expect(reqSentBtn).toHaveAttribute("disabled");
+		});
+	});
 
-    describe("Check element rendering and interaction: request-sent case", () => {
-        beforeEach(async () => {
-          await renderWithMock(<FriendButtons currentUser={loggedInUser} user={testUsers[1]} />);
-        });
+	describe("Check element rendering and interaction: request-received case", () => {
+		beforeEach(async () => {
+			await renderWithMock(
+				<FriendButtons currentUser={loggedInUser} user={testUsers[2]} />
+			);
+		});
 
-        it("should render request sent button", async () => {
-            const reqSentBtn = await screen.findByTestId("sent-req-btn");
-            expect(reqSentBtn).toBeInTheDocument();
-            expect(reqSentBtn).toHaveAttribute("disabled");
-        });
-    });
+		it("should render accept request button", async () => {
+			const reqAcceptBtn = await screen.findByTestId("accept-req-btn");
+			expect(reqAcceptBtn).toBeInTheDocument();
+		});
 
-    describe("Check element rendering and interaction: request-received case", () => {
-        beforeEach(async () => {
-          await renderWithMock(<FriendButtons currentUser={loggedInUser} user={testUsers[2]} />);
-        });
+		it("should render reject request button", async () => {
+			const reqRejectBtn = await screen.findByTestId("reject-req-btn");
+			expect(reqRejectBtn).toBeInTheDocument();
+		});
 
-        it("should render accept request button", async () => {
-            const reqAcceptBtn = await screen.findByTestId("accept-req-btn");
-            expect(reqAcceptBtn).toBeInTheDocument();
-        });
+		it("handles accept friend press", async () => {
+			const accept = await screen.findByTestId("accept-req-btn");
+			fireEvent.click(accept);
+			const del = await screen.findByTestId("del-friend-btn");
+			expect(del).toBeInTheDocument();
+			expect(accept).not.toBeInTheDocument();
+		});
 
-        it("should render reject request button", async () => {
-            const reqRejectBtn = await screen.findByTestId("reject-req-btn");
-            expect(reqRejectBtn).toBeInTheDocument();
-        });
+		it("handles reject friend press", async () => {
+			const reject = await screen.findByTestId("reject-req-btn");
+			fireEvent.click(reject);
+			const add = await screen.findByTestId("add-friend-btn");
+			expect(add).toBeInTheDocument();
+			expect(reject).not.toBeInTheDocument();
+		});
+	});
 
-        it("handles accept friend press", async () => {
-            const accept = await screen.findByTestId("accept-req-btn");
-            fireEvent.click(accept);
+	describe("Check element rendering and interaction: no-relation case", () => {
+		beforeEach(async () => {
+			await renderWithMock(
+				<FriendButtons currentUser={loggedInUser} user={testUsers[3]} />
+			);
+		});
 
-            expect(accept).not.toBeInTheDocument();
+		it("should render add friend button", async () => {
+			const addFriendBtn = await screen.findByTestId("add-friend-btn");
+			expect(addFriendBtn).toBeInTheDocument();
+		});
 
-            const del = await screen.findByTestId("del-friend-btn");
-            expect(del).toBeInTheDocument();
-        });
-
-        it("handles reject friend press", async () => {
-            const reject = await screen.findByTestId("reject-req-btn");
-            fireEvent.click(reject);
-
-            expect(reject).not.toBeInTheDocument();
-
-            const add = await screen.findByTestId("add-friend-btn");
-            expect(add).toBeInTheDocument();
-        });
-    });
-
-    describe("Check element rendering and interaction: no-relation case", () => {
-        beforeEach(async () => {
-          await renderWithMock(<FriendButtons currentUser={loggedInUser} user={testUsers[3]} />);
-        });
-
-        it("should render add friend button", async () => {
-            const addFriendBtn = await screen.findByTestId("add-friend-btn");
-            expect(addFriendBtn).toBeInTheDocument();
-        });
-
-        it("handles add friend press", async () => {
-            const addFriendBtn = await screen.findByTestId("add-friend-btn");
-            fireEvent.click(addFriendBtn);
-
-            expect(addFriendBtn).not.toBeInTheDocument();
-
-            const reqSentBtn = await screen.findByTestId("sent-req-btn");
-            expect(reqSentBtn).toBeInTheDocument();
-            expect(reqSentBtn).toHaveAttribute("disabled");
-        });
-    });
+		it("handles add friend press", async () => {
+			const addFriendBtn = await screen.findByTestId("add-friend-btn");
+			fireEvent.click(addFriendBtn);
+			const reqSentBtn = await screen.findByTestId("sent-req-btn");
+			expect(reqSentBtn).toBeInTheDocument();
+			expect(reqSentBtn).toBeDisabled();
+      expect(addFriendBtn).not.toBeInTheDocument();
+		});
+	});
 });
