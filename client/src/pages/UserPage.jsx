c07b133bcdd776fba2f208895f5f37d31319364a
Krishi Wali <krishi.wali@kcl.ac.uk>
2022-03-29 17:41:38 +0100

    Fix alignment of text on tabs

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -104,8 +104,8 @@ export default function UserPage() {
       <Box sx={{ width: '100%' }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="User type tab">
-            <Tab label="All" {...a11yProps(0)} />
-            <Tab label="Friends" {...a11yProps(1)} />
+            <Tab label="All" sx={{ justifyContent: "start" }} {...a11yProps(0)} />
+            <Tab label="Friends" sx={{ justifyContent: "start" }} {...a11yProps(1)} />
             <Tab label="Same Course" sx={{display : {xs: "none", md: "block"}}} {...a11yProps(2)} />
             <Tab label="Received Requests"  sx={{display : {xs: "none", md: "block"}}}  {...a11yProps(3)} />
             <Tab label="Sent Requests"  sx={{display : {xs: "none", md: "block"}}}  {...a11yProps(4)} />
