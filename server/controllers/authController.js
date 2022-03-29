f5b66aec9c0998c93b4427515cfc175fbe46a8cf
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-29 18:47:38 +0100

    Add jsdoc to authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,6 +17,11 @@ const COOKIE_MAX_AGE = 432000; // 432000 = 5 days
 const SALT_ROUNDS = 10;
 
 // Helper method to generate JWT
+/**
+ * Creates a token cookie to keep the user logged in.
+ * @param id The mongoose object id of the user.
+ * @returns {*} Javascript function.
+ */
 const createToken = (id) => {
 	return jwt.sign({ id }, "kekw", {
 		expiresIn: COOKIE_MAX_AGE,
@@ -24,7 +29,14 @@ const createToken = (id) => {
 };
 module.exports.createToken = createToken;
 
-// POST /signup
+
+/**
+ * POST /signup. Sends signup infrmation for server to verify and create an account.
+ * On Success, response object contains logged-in user's information.
+ * @param req contains information regarding the incoming request. (User signup form)
+ * @param res contains information regarding the response. (User information)
+ * @returns {Promise<void>} Javascript async function.
+ */
 module.exports.signupPost = async (req, res) => {
 	const { firstName, lastName, uniEmail, password } = req.body;
 	const processedEmail = uniEmail.toLowerCase()
@@ -102,7 +114,13 @@ module.exports.signupPost = async (req, res) => {
 	}
 };
 
-// POST /login
+/**
+ * POST /login. Sends login information to server to verify.
+ * On Success, response object contains logged-in user's information.
+ * @param req contains information regarding the incoming request. (User login form)
+ * @param res contains information regarding the response. (User information)
+ * @returns {Promise<void>} Javascript async function.
+ */
 module.exports.loginPost = async (req, res) => {
 	const { uniEmail, password } = req.body;
 
@@ -137,13 +155,23 @@ module.exports.loginPost = async (req, res) => {
 	}
 };
 
-// GET /logout
+/**
+ * GET /logout. Logs out the user when user reaches this endpoint.
+ * @param req contains information regarding the incoming request.
+ * @param res contains information regarding the response.
+ * @returns {Promise<void>} Javascript async function.
+ */
 module.exports.logoutGet = (req, res) => {
 	res.cookie("jwt", "", { maxAge: 1 });
 	res.status(200).json(jsonResponse(null, []));
 };
 
-// GET /verify
+/**
+ * GET /verify. Handles verification codes sent by the email server.
+ * @param req contains information regarding the incoming request. (Verification Code)
+ * @param res contains information regarding the response. (Success message)
+ * @returns {Promise<void>} Javascript async function.
+ */
 module.exports.verifyGet = async (req, res) => {
 	try {
 		// Get code from query param
@@ -172,6 +200,13 @@ module.exports.verifyGet = async (req, res) => {
 	}
 };
 
+/**
+ * GET /me. Returns logged-in user's information.
+ * On Success, response object contains logged-in user's information.
+ * @param req contains information regarding the incoming request.
+ * @param res contains information regarding the response. (User information)
+ * @returns {Promise<void>} Javascript async function.
+ */
 module.exports.meGet = async (req, res) => {
 	res.status(200).json(jsonResponse(req.user, []));
 };
