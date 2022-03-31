bcec03b63a84ec0c4a82175c965e6d26ca6c0cfa
Kieran Woolley <kieran.woolley1@gmail.com>
2022-03-31 17:10:42 +0100

    Import auth controller methods to routes individually

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,17 +5,17 @@
 
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/authController");
+const { signup, login, logout, verify, getMe } = require("../controllers/authController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 
-router.post("/signup", authController.signup);
+router.post("/signup", signup);
 
-router.post("/login", authController.login);
+router.post("/login", login);
 
-router.get("/logout", authController.logout);
+router.get("/logout", logout);
 
-router.get("/verify", authController.verify);
+router.get("/verify", verify);
 
-router.get("/me", checkAuthenticated, authController.getMe);
+router.get("/me", checkAuthenticated, getMe);
 
 module.exports = router;
