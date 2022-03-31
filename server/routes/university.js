0b54a2d18636bfec5756ad20b45e57e14debc158
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-31 17:17:13 +0100

    Improve imports for university routes

--- server/routes/university.js
+++ server/routes/university.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const universityController = require("../controllers/universityController");
+const { getUniversity, getSearchResults } = require("../controllers/universityController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 
-router.get("/university", checkAuthenticated, universityController.universityGet);
-router.get("/search/:query", checkAuthenticated, universityController.getSearchResults);
+router.get("/university", checkAuthenticated, getUniversity);
+router.get("/search/:query", checkAuthenticated, getSearchResults);
 
 module.exports = router;
