c742d38697926cb6b95952bf09905daea1315eee
Kieran Woolley <kieran.woolley1@gmail.com>
2022-03-31 17:12:05 +0100

    Import comment controller methods to routes individually

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const commentController = require("../controllers/commentController");
+const { createComment, deleteComment, getComment, upvoteComment, downvoteComment, createReplyToComment } = require("../controllers/commentController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 const { checkIsMemberOfPact } = require("../middleware/pactMiddleware");
 const { checkValidPost, checkValidPostComment } = require("../middleware/postMiddleware");
 
-router.post("/pact/:pactId/post/:postId/comment", checkAuthenticated, checkIsMemberOfPact, checkValidPost, commentController.createComment);
+router.post("/pact/:pactId/post/:postId/comment", checkAuthenticated, checkIsMemberOfPact, checkValidPost, createComment);
 
-router.delete("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, commentController.deleteComment);
-router.get("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, commentController.getComment);
+router.delete("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, deleteComment);
+router.get("/pact/:pactId/post/:postId/comment/:commentId", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, getComment);
 
-router.put("/pact/:pactId/post/:postId/comment/:commentId/upvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, commentController.upvoteComment);
-router.put("/pact/:pactId/post/:postId/comment/:commentId/downvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, commentController.downvoteComment);
+router.put("/pact/:pactId/post/:postId/comment/:commentId/upvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, upvoteComment);
+router.put("/pact/:pactId/post/:postId/comment/:commentId/downvote", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, downvoteComment);
 
-router.post("/pact/:pactId/post/:postId/comment/:commentId/reply", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, commentController.createReplyToComment);
+router.post("/pact/:pactId/post/:postId/comment/:commentId/reply", checkAuthenticated, checkIsMemberOfPact, checkValidPost, checkValidPostComment, createReplyToComment);
 
 module.exports = router;
