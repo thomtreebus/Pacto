667505c8959a3942885d6134e3421584f8e999fe
Chirag Mahesh <chirag.mahesh@kcl.ac.uk>
2022-03-31 01:35:32 +0100

    Remove unnecessary return

--- client/src/components/cards/CommentCard.jsx
+++ client/src/components/cards/CommentCard.jsx
@@ -78,7 +78,6 @@ export default function CommentCard({ comment, post, postUpdaterFunc }) {
       setIsError(true);
       setErrorMessage(err.message);
       setIsButtonDisabled(false);
-      return;
     }
   };
 
