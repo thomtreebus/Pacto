204a23423ea8cd30c45fba344e010ffac3c9febe
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-31 17:09:17 +0100

    Rename post controllers

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const postController = require("../controllers/postController");
+const { createPost, getPost, upvotePost, downvotePost, deletePost } = require("../controllers/postController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 const { checkIsMemberOfPact } = require("../middleware/pactMiddleware");
 
-router.post("/pact/:pactId/post", checkAuthenticated, checkIsMemberOfPact, postController.postPost);
-router.get("/pact/:pactId/post/:postId", checkAuthenticated, checkIsMemberOfPact, postController.postGet);
-router.put("/pact/:pactId/post/upvote/:postId", checkAuthenticated, checkIsMemberOfPact, postController.upvotePostPut);
-router.put("/pact/:pactId/post/downvote/:postId", checkAuthenticated, checkIsMemberOfPact, postController.downvotePostPut);
-router.delete("/pact/:pactId/post/:postId", checkAuthenticated, checkIsMemberOfPact, postController.postDelete);
+
+router.post("/pact/:pactId/post", checkAuthenticated, checkIsMemberOfPact, createPost);
+router.get("/pact/:pactId/post/:postId", checkAuthenticated, checkIsMemberOfPact, getPost);
+router.put("/pact/:pactId/post/upvote/:postId", checkAuthenticated, checkIsMemberOfPact, upvotePost);
+router.put("/pact/:pactId/post/downvote/:postId", checkAuthenticated, checkIsMemberOfPact, downvotePost);
+router.delete("/pact/:pactId/post/:postId", checkAuthenticated, checkIsMemberOfPact, deletePost);
 
 module.exports = router;
