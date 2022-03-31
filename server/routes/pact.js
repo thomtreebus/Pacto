0d8b69b1f3356cb4d4752eef5ade759fd1bdd431
Thom Treebus <thom.treebus@kcl.ac.uk>
2022-03-31 17:31:36 +0100

    Rename edit to update

--- server/routes/pact.js
+++ server/routes/pact.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createPact, getPact, editPact, joinPact, banMember, promoteMember, revokeBan, leavePact, deleteAllComments, deletePact } = require("../controllers/pactController");
+const { createPact, getPact, updatePact, joinPact, banMember, promoteMember, revokeBan, leavePact, deleteAllComments, deletePact } = require("../controllers/pactController");
 const { checkAuthenticated } = require("../middleware/authMiddleware");
 const { checkIsMemberOfPact, checkIsModeratorOfPact } = require("../middleware/pactMiddleware");
 
 router.post("/pact", checkAuthenticated, createPact);
 router.post("/pact/:id/join", checkAuthenticated, joinPact);
 router.get("/pact/:pactId", checkAuthenticated, checkIsMemberOfPact, getPact);
-router.put("/pact/:pactId", checkAuthenticated, checkIsMemberOfPact, editPact);
+router.put("/pact/:pactId", checkAuthenticated, checkIsMemberOfPact, updatePact);
 router.put("/pact/:pactId/:userId/ban", checkAuthenticated, checkIsMemberOfPact, checkIsModeratorOfPact, banMember);
 router.put("/pact/:pactId/:userId/promote", checkAuthenticated, checkIsMemberOfPact, checkIsModeratorOfPact, promoteMember);
 router.put("/pact/:pactId/:userId/revokeban", checkAuthenticated, checkIsMemberOfPact, checkIsModeratorOfPact, revokeBan);
