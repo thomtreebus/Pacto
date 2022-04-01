1440a9d10f1065815c291478cfe713ffe4aea424
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2022-04-01 19:55:24 +0100

    Add additional null check

--- client/src/components/PactPage/PactPageFeedTab.jsx
+++ client/src/components/PactPage/PactPageFeedTab.jsx
@@ -164,7 +164,7 @@ export default function PactPageFeedTab({pact}){
               {"Create Post"}
             </DialogTitle>
             <DialogContent>
-              <CreatePostCard pactID={pact._id} />
+              { pact && <CreatePostCard pactID={pact._id} /> }
             </DialogContent>
           </Dialog>
         </Box>
